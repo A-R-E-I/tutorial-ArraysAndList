fe5d80c9a7e722fa80b9342c02ba02a8c0e66b66
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2024-11-22 14:56:20 -0500

    Add files via upload

--- arraydisp.js
+++ arraydisp.js
@@ -1,3 +1,49 @@
-/**
- * 
- */
+window.addEventListener("load", Initialize);
+window.addEventListener("load", addListener);
+
+let countvalue = 0;
+let infoarray = [];
+
+function Initialize()
+{
+	document.getElementById("txtinput").focus();
+	document.getElementById("txtinput").value = "";
+}
+
+function addListener()
+{
+	document.getElementById("btnSubmit").addEventListener("click", InsertArray);
+	document.getElementById("btnDisplay").addEventListener("click", DisplayArray);
+}
+
+function InsertArray()
+{
+	let inputvalue = "";
+	inputvalue = document.getElementById("txtinput").value;
+	infoarray.push(inputvalue);
+	
+	alert("Element: " + infoarray[countvalue] + "has been added.");
+	countvalue = countvalue + 1;
+	Initialize();
+}
+
+function DisplayArray()
+{
+	let disparray = "";
+	let arraylen = infoarray.length;
+	
+	for(let i=0; i<arraylen; i++)
+	{
+		if(disparray == "")
+		{
+			disparray = infoarray[i];
+		}
+		else
+		{
+			disparray = disparray + " " + infoarray[i];
+		}
+	}
+	
+	document.getElementById("lbldispresult").textContent = disparray;
+	Initialize();
+}
